04e4ae410360cef3423d4ac0a8cc4c18c8986bbb
Anselm von Wangenheim <anselm.w@gmx.de>
2021-03-08 19:08:33 +0100

    add some tests

---
+++ src/test/scala/TestAlternativeVote.scala
@@ -0,0 +1,53 @@
+
+import AlternativeVote.{Candidate, candidate}
+import org.junit.Test
+import org.junit.Assert._
+
+class TestAlternativeVote {
+  /**
+   * turns "ab,ac,bcd" into Set(Seq(a, b), Seq(a, c), Seq(b, c, d))
+   */
+  def voteLists(s: String): Seq[Seq[Candidate]] =
+    s.split(",").toSeq
+      .map(candidates)
+
+  def candidates(s: String): Seq[Candidate] =
+    s.map((c: Char) => candidate(c.toString)).toSeq
+
+  val abc = new AlternativeVote(candidates("abc").toSet)
+
+  val noTie: Any => Nothing = x => throw new Error(s"there should not be a tie: $x")
+
+  @Test def t1(): Unit = {
+    assertEquals("a", abc.getWinner(voteLists("a,a,ab,abc"), noTie))
+  }
+
+  @Test def t2(): Unit = {
+    assertEquals("a", abc.getWinner(voteLists("a,ab,cba"), noTie))
+  }
+
+  @Test def t3(): Unit = {
+    assertEquals("a", abc.getWinner(voteLists("a,ba,abc"), noTie))
+  }
+
+  @Test def t4(): Unit = {
+    assertEquals("b", abc.getWinner(voteLists("a,ba,bca"), noTie))
+  }
+
+  @Test def t5(): Unit = {
+    assertEquals("e", abc.getWinner(voteLists("a,b,c"), _ => candidate("e")))
+  }
+
+  @Test def t6(): Unit = {
+    assertEquals("e", abc.getWinner(voteLists("a,ba,cba"), _ => candidate("e")))
+  }
+
+  @Test def t7(): Unit = {
+    assertEquals("a", abc.getWinner(voteLists("ba,bc,ca,da,ea"), noTie))
+  }
+
+  @Test def t8(): Unit = {
+    assertEquals("a", abc.getWinner(voteLists("a,a,a"), noTie))
+  }
+
+}
