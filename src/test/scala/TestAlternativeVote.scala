b856e10bfe139a066cf773771d29287434d18027
Anselm von Wangenheim <anselm.w@gmx.de>
2021-04-17 20:26:34 +0200

    make tests scala-2-compatible

--- src/test/scala/TestAlternativeVote.scala
+++ src/test/scala/TestAlternativeVote.scala
@@ -17,70 +17,70 @@ class TestAlternativeVote {
   val noTie: Any => Nothing = x => throw new Error(s"there should not be a tie: $x")
 
   @Test def t1(): Unit = {
-    assertEquals("a", AlternativeVote(voteLists("a,a,ab,abc")).getWinner(tieBreaking = noTie))
+    assertEquals("a", new AlternativeVote(voteLists("a,a,ab,abc")).getWinner(tieBreaking = noTie))
   }
 
   @Test def t2(): Unit = {
-    assertEquals("a", AlternativeVote(voteLists("a,ab,cba")).getWinner(tieBreaking = noTie))
+    assertEquals("a", new AlternativeVote(voteLists("a,ab,cba")).getWinner(tieBreaking = noTie))
   }
 
   @Test def t3(): Unit = {
-    assertEquals("a", AlternativeVote(voteLists("a,ba,abc")).getWinner(tieBreaking = noTie))
+    assertEquals("a", new AlternativeVote(voteLists("a,ba,abc")).getWinner(tieBreaking = noTie))
   }
 
   @Test def t4(): Unit = {
-    assertEquals("b", AlternativeVote(voteLists("a,ba,bca")).getWinner(tieBreaking = noTie))
+    assertEquals("b", new AlternativeVote(voteLists("a,ba,bca")).getWinner(tieBreaking = noTie))
   }
 
   @Test def t5(): Unit = {
-    assertEquals("c", AlternativeVote(voteLists("a,b,c")).getWinner(tieBreaking = _ => Set(candidate("c"))))
+    assertEquals("c", new AlternativeVote(voteLists("a,b,c")).getWinner(tieBreaking = _ => Set(candidate("c"))))
   }
 
   @Test def t6(): Unit = {
-    assertEquals("a", AlternativeVote(voteLists("a,ba,cba")).getWinner(tieBreaking = _ => Set(candidate("a"), candidate("c"))))
+    assertEquals("a", new AlternativeVote(voteLists("a,ba,cba")).getWinner(tieBreaking = _ => Set(candidate("a"), candidate("c"))))
   }
 
   @Test def t7(): Unit = {
-    assertEquals("b", AlternativeVote(voteLists("ba,bc,ca,da,ea")).getWinner(tieBreaking = noTie))
+    assertEquals("b", new AlternativeVote(voteLists("ba,bc,ca,da,ea")).getWinner(tieBreaking = noTie))
   }
 
   @Test def t8(): Unit = {
-    assertEquals("a", AlternativeVote(voteLists("ba,bc,ca,da,ea,ab,ab")).getWinner(tieBreaking = noTie))
+    assertEquals("a", new AlternativeVote(voteLists("ba,bc,ca,da,ea,ab,ab")).getWinner(tieBreaking = noTie))
   }
 
   @Test def t9(): Unit = {
-    assertEquals("a", AlternativeVote(voteLists("a,a,a")).getWinner(tieBreaking = noTie))
+    assertEquals("a", new AlternativeVote(voteLists("a,a,a")).getWinner(tieBreaking = noTie))
   }
 
   @Test def t10(): Unit = {
-    assertEquals("a", AlternativeVote(voteLists("a,a,ba,bac")).getWinner())
+    assertEquals("a", new AlternativeVote(voteLists("a,a,ba,bac")).getWinner())
   }
 
   @Test def t11(): Unit = {
-    assertEquals("a", AlternativeVote(voteLists("a,ba,c")).getWinner())
+    assertEquals("a", new AlternativeVote(voteLists("a,ba,c")).getWinner())
   }
 
   @Test def t12(): Unit = {
-    assertEquals("a", AlternativeVote(voteLists("a,ab,ba,ba,c")).getWinner())
+    assertEquals("a", new AlternativeVote(voteLists("a,ab,ba,ba,c")).getWinner())
   }
 
   @Test def t13(): Unit = {
-    assertEquals(Set(), AlternativeVote(voteLists("a,ab,ba,ba,c")).invalidCandidates(candidates("abc").toSet))
+    assertEquals(Set(), new AlternativeVote(voteLists("a,ab,ba,ba,c")).invalidCandidates(candidates("abc").toSet))
   }
 
   @Test def t14(): Unit = {
-    assertEquals(Set(candidate("d")), AlternativeVote(voteLists("a,ab,bad,ba,c")).invalidCandidates(candidates("abc").toSet))
+    assertEquals(Set(candidate("d")), new AlternativeVote(voteLists("a,ab,bad,ba,c")).invalidCandidates(candidates("abc").toSet))
   }
 
   @Test def rnd1(): Unit = {
-    def rndSeq(seed: Int) = scala.util.Random(seed).alphanumeric.map {
+    def rndSeq(seed: Int) = new scala.util.Random(seed).alphanumeric.map {
       case c if c <= '9' => ','
       case c => c.toLower
     }.take(500).mkString("")
 
     "uoozbnhfycihpnuedkvdcugj".zipWithIndex.foreach {
       case (c, i) =>
-        assertEquals(c.toString, AlternativeVote(voteLists(rndSeq(i))).getWinner())
+        assertEquals(c.toString, new AlternativeVote(voteLists(rndSeq(i))).getWinner())
     }
   }
 }
--- src/test/scala/TestIterativeAlternativeVote.scala
+++ src/test/scala/TestIterativeAlternativeVote.scala
@@ -14,32 +14,32 @@ class TestIterativeAlternativeVote {
     s.map((c: Char) => candidate(c.toString)).toSeq
 
   @Test def rnd1(): Unit = {
-    def rndSeq(seed: Int) = scala.util.Random(seed).alphanumeric.map {
+    def rndSeq(seed: Int) = new scala.util.Random(seed).alphanumeric.map {
       case c if c <= '9' => ','
       case c => c.toLower
     }.take(500).mkString("")
 
-    assertEquals("uwznvfkxhberyiapojgdsqtcml", IterativeAlternativeVote(voteLists(rndSeq(0))).winners.mkString(""))
-    assertEquals("opurhbqnmsifavzdetlkyxgcwj", IterativeAlternativeVote(voteLists(rndSeq(1))).winners.mkString(""))
-    assertEquals("ochwdlfevtrismpgyzjxuqnakb", IterativeAlternativeVote(voteLists(rndSeq(2))).winners.mkString(""))
-    assertEquals("zcxhyswvkptmeqrgaunfobijdl", IterativeAlternativeVote(voteLists(rndSeq(3))).winners.mkString(""))
-    assertEquals("bjyihrszwdlkafemqgonuvxtcp", IterativeAlternativeVote(voteLists(rndSeq(4))).winners.mkString(""))
-    assertEquals("ngpsozcfjwlimtxdeqvyrbahuk", IterativeAlternativeVote(voteLists(rndSeq(5))).winners.mkString(""))
-    assertEquals("hiwobzfdypuvrkatgjexlnsqcm", IterativeAlternativeVote(voteLists(rndSeq(6))).winners.mkString(""))
-    assertEquals("fdtkiouzxapmrenbwhylgjcsvq", IterativeAlternativeVote(voteLists(rndSeq(7))).winners.mkString(""))
+    assertEquals("uwznvfkxhberyiapojgdsqtcml", new IterativeAlternativeVote(voteLists(rndSeq(0))).winners.mkString(""))
+    assertEquals("opurhbqnmsifavzdetlkyxgcwj", new IterativeAlternativeVote(voteLists(rndSeq(1))).winners.mkString(""))
+    assertEquals("ochwdlfevtrismpgyzjxuqnakb", new IterativeAlternativeVote(voteLists(rndSeq(2))).winners.mkString(""))
+    assertEquals("zcxhyswvkptmeqrgaunfobijdl", new IterativeAlternativeVote(voteLists(rndSeq(3))).winners.mkString(""))
+    assertEquals("bjyihrszwdlkafemqgonuvxtcp", new IterativeAlternativeVote(voteLists(rndSeq(4))).winners.mkString(""))
+    assertEquals("ngpsozcfjwlimtxdeqvyrbahuk", new IterativeAlternativeVote(voteLists(rndSeq(5))).winners.mkString(""))
+    assertEquals("hiwobzfdypuvrkatgjexlnsqcm", new IterativeAlternativeVote(voteLists(rndSeq(6))).winners.mkString(""))
+    assertEquals("fdtkiouzxapmrenbwhylgjcsvq", new IterativeAlternativeVote(voteLists(rndSeq(7))).winners.mkString(""))
   }
 
   @Test def rnd2(): Unit = {
-    def rndSeq(seed: Int) = scala.util.Random(seed).alphanumeric.map {
+    def rndSeq(seed: Int) = new scala.util.Random(seed).alphanumeric.map {
       case c if c <= '9' => ','
       case c => c.toLower
     }.take(50).mkString("")
 
-    assertEquals("wczhfuntrvi", IterativeAlternativeVote(voteLists(rndSeq(0))).winners.take(11).mkString(""))
-    assertEquals("ihqzynavubos", IterativeAlternativeVote(voteLists(rndSeq(1))).winners.take(12).mkString(""))
-    assertEquals("arhi", IterativeAlternativeVote(voteLists(rndSeq(2))).winners.take(4).mkString(""))
-    assertEquals("ziwpco", IterativeAlternativeVote(voteLists(rndSeq(3))).winners.take(6).mkString(""))
-    assertEquals("rws", IterativeAlternativeVote(voteLists(rndSeq(4))).winners.take(3).mkString(""))
-    assertEquals("fpqrc", IterativeAlternativeVote(voteLists(rndSeq(5))).winners.take(5).mkString(""))
+    assertEquals("wczhfuntrvi", new IterativeAlternativeVote(voteLists(rndSeq(0))).winners.take(11).mkString(""))
+    assertEquals("ihqzynavubos", new IterativeAlternativeVote(voteLists(rndSeq(1))).winners.take(12).mkString(""))
+    assertEquals("arhi", new IterativeAlternativeVote(voteLists(rndSeq(2))).winners.take(4).mkString(""))
+    assertEquals("ziwpco", new IterativeAlternativeVote(voteLists(rndSeq(3))).winners.take(6).mkString(""))
+    assertEquals("rws", new IterativeAlternativeVote(voteLists(rndSeq(4))).winners.take(3).mkString(""))
+    assertEquals("fpqrc", new IterativeAlternativeVote(voteLists(rndSeq(5))).winners.take(5).mkString(""))
   }
 }
