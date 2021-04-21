ff9182b8c3485c41bd5d2992b589ac5013846bf6
Anselm von Wangenheim <anselm.w@gmx.de>
2021-04-21 15:05:39 +0200

    don't rely on exception in iterative alternative vote

--- src/main/scala/IterativeAlternativeVote.scala
+++ src/main/scala/IterativeAlternativeVote.scala
@@ -4,13 +4,12 @@ class IterativeAlternativeVote(var voteLists: Seq[Seq[Candidate]]) {
   voteLists = voteLists
     .map(_.distinct) // should not have same candidate multiple times on list!
     .sortBy(_.hashCode) // order should never depend on input order!
-  
+
   def winners: LazyList[Candidate] = {
-    try {
+    if (voteLists.flatten.isEmpty) LazyList()
+    else {
       val winner = new AlternativeVote(voteLists).getWinner()
       winner #:: new IterativeAlternativeVote(voteLists.map(_.filter(_ != winner))).winners
-    } catch {
-      case _: NoSuchElementException => LazyList()
     }
   }
 }
